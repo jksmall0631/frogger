baf02a4bdfd6b120b8cf09682eaeaf23ca2194d7
Dylan Avery <fossilfuelmusic@gmail.com>
2016-11-03 16:28:04 -0600

    write a bunch of tests and split frog movement into prototypes

---
+++ test/carLeft-test.js
@@ -0,0 +1,51 @@
+const assert = require('chai').assert;
+var CarLeft = require('../lib/carLeft.js');
+
+
+describe('CarLeft', function () {
+  it('should be a function', function () {
+    assert.isFunction(CarLeft);
+  });
+
+  it('should instantiate a wayward car heading left', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.isObject(carLeft);
+  });
+
+  it('should have an x coordinate', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.equal(carLeft.x, 0);
+  });
+
+  it('should have a y coordinate', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.equal(carLeft.y, 0);
+  });
+
+  it('should have a velocity', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.equal(carLeft.vx, 2);
+  });
+
+  it('should have a height', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.equal(carLeft.height, 50);
+  });
+
+  it('should have a width', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.equal(carLeft.width, 50);
+  });
+
+  it('should spawn a new car when drawn', function () {
+    var carLeft = new CarLeft(0,0,2,50);
+    assert.isFunction(carLeft.draw);
+  });
+
+  it('should always be moving 2px to the left', function () {
+    var carLeft = new CarLeft(50,0,2,50);
+    carLeft.move()
+    assert.equal(carLeft.x, 52);
+  });
+
+});
---
+++ test/carRight-test.js
@@ -0,0 +1,51 @@
+const assert = require('chai').assert;
+var CarRight = require('../lib/carRight.js');
+
+
+describe('CarRight', function () {
+  it('should be a function', function () {
+    assert.isFunction(CarRight);
+  });
+
+  it('should instantiate a wayward car heading left', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.isObject(carRight);
+  });
+
+  it('should have an x coordinate', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.equal(carRight.x, 0);
+  });
+
+  it('should have a y coordinate', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.equal(carRight.y, 0);
+  });
+
+  it('should have a velocity', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.equal(carRight.vx, 2);
+  });
+
+  it('should have a height', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.equal(carRight.height, 50);
+  });
+
+  it('should have a width', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.equal(carRight.width, 50);
+  });
+
+  it('should spawn a new car when drawn', function () {
+    var carRight = new CarRight(0,0,2,50);
+    assert.isFunction(carRight.draw);
+  });
+
+  it('should always be moving 2px to the left', function () {
+    var carRight = new CarRight(50,0,2,50);
+    carRight.move()
+    assert.equal(carRight.x, 52);
+  });
+
+});
